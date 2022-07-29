62b343414d9a57ade03f5d016f96570ce789d601
Dante S <106348992+dntalx@users.noreply.github.com>
2022-07-28 18:02:16 -0600

    Delete config.py

--- Vacation_Itinerary/config.py
+++
@@ -1,8 +0,0 @@
-#google_key="AIzaSyB3v9W-Fxi2A-OpxdVrLBTrM-IZ38slndg"
-g_key = "AIzaSyB3v9W-Fxi2A-OpxdVrLBTrM-IZ38slndg"
-#g_key = "AIzaSyAlXeCup0sKjNHFcRt49Dxk8DSwj2nZbKo"
-weather_api_key = "ce8bdb08086a37bd077736b31b22a743"
-
-
-
-
